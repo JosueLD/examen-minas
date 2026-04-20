84c949495f52019e57ae386f1b46bff7baa39c1d
JosueLD <130570710+JosueLD@users.noreply.github.com>
2026-04-20 05:49:49 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 # --- DATOS DEL DOCENTE ---
 DOCENTE_INFO = "MSc. Ing. Josué Loayza Díaz - CIP: 169617"
-URL_HOJA = st.secrets["connections"]["spreadsheet"]["gsheets"]
+URL_HOJA = st.secrets["connections"]["gsheets"]["spreadsheet"]
 
 class PDF(FPDF):
     def footer(self):
